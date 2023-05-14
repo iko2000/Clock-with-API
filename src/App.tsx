d23cd332123a4ebbf29a1bf6cb3ebf05d6a773ca
iko2000 <123463655+iko2000@users.noreply.github.com>
2023-05-14 16:14:05 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { url } from "inspector";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import down from "./assets/desktop/arrowdown.svg";
+import up from "./assets/desktop/arrowup.svg";
+import sun from "./assets/desktop/sun.svg"
+import moon from "./assets/desktop/moon.svg"
+
+
 
 function App() {
+  const [isday, setIsday] = useState(true);
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState('');
+  const [time, setTime] = useState('');
+  const [location, setLocation] = useState('');
+  const [day, setDay] = useState('');
+  const [week, setWeek] = useState('');
+  const [year, setYear] = useState('');
+  const [weekpassed, setWeekpassed] = useState('');
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [city, setCity] = useState('');
+  const [ismorning, setIsmorning] = useState(true);
+   
+  let num = time.slice(0,2);
+ 
+
+   function generator(num:number) {
+     if(num > 16) {
+      setIsmorning(false)
+     } else {
+      setIsmorning(true)
+     }
+   }
+
+  useEffect(() => {
+    axios(`http://worldtimeapi.org/api/ip`)
+    .then((response) => response.data)
+    .then((data) => {
+      console.log(data)
+      setData(data);
+      let formatedtime = data.datetime.slice(11,16)
+      setTime(formatedtime)
+      setLocation(data.timezone)
+      setWeek(data.day_of_week);
+      setYear(data.day_of_year);
+      setWeekpassed(data.week_number);
+    }).catch(
+      function (error) {
+        alert(error + 'Show error notification!')
+      })
+
+      generator(num)
+
+    axios('https://ipapi.co/json/').then((response) => response.data).then((data) => {
+      console.log(data) 
+       setCity(` ${data.city}, ${data.country_name}`)
+    
+    
+    })
+      
+  }, [])
+
+
+  useEffect( () => {
+    axios('https://api.quotable.io/quotes/random')
+    .then((response) => response.data)
+    .then((data) => {
+      console.log(data);
+      setQuote(data[0].content)
+      setAuthor(data[0].author)
+    }).catch(
+      function (error) {
+        alert(error + 'Show error notification!')
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={ismorning ? "box day" : "box night"}>
+        <div className={show ? "activepositioner" : "positioner"}>
+          <div className="place">
+          <div className="display">
+            <div className={show ? "quotesactive" : "quotes"}>
+              <p>{quote}</p>
+              <p>{author}</p>
+
+            </div>
+            <div className={show ? "showntime" : "time"}>
+              <div className="welcomer">
+                <p> <img src={ismorning ? sun : moon}/>{ismorning ? 'GOOD MORNING' : 'GOOD EVENING'} IT'S CURRENTLY</p>
+              </div>
+              <div className="clock">
+                <p>{time}</p>
+              </div>
+              <div className="location">
+                <p>IN {city.toUpperCase()}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="clicker">
+            <button onClick={() => {
+              setShow(!show);
+            }} > {show ? "LESS" : "MORE"}         <div className="btnimg"><img src={show ? up : down}/></div></button>
+          </div>
+          </div>
+        </div>
+        <div className={show ? "active" : "passive"}>
+
+        <div className="floors"> 
+         <div className="side"><label>CURRENT TIMEZONE</label> <p>{location}</p></div>
+         <div className="side"><label>DAY OF THE YEAR</label> <p>{year}</p></div>
+
+        </div>
+        <div className="floors"> 
+        <div className="side"><label>DAY OF THE WEEK </label> <p>{week}</p></div>
+         <div className="side"><label>WEEK NUMBERS </label> <p>{weekpassed}</p></div>
+         </div>
+
+
+        </div>
+      </div>
+     
     </div>
   );
 }
